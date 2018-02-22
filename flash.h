a9db8c798241ab9d80435b3ad25b24102560cbb6
Antti Alhonen <antti.alhonen@iki.fi>
2018-02-22 08:43:56 +0200

    Super fancy, reliable and quick new SPI reflashing thing

---
+++ flash.h
@@ -0,0 +1,26 @@
+/*
+	Originally from: PULUROBOT RN1-BRAIN RobotBoard main microcontroller firmware project
+
+	(c) 2017-2018 Pulu Robotics and other contributors
+	Maintainer: Antti Alhonen <antti.alhonen@iki.fi>
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License version 2, as 
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	GNU General Public License version 2 is supplied in file LICENSING.
+
+*/
+
+#ifndef _FLASHER_H
+#define _FLASHER_H
+
+void run_flasher();
+
+
+#endif
